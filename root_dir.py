88aa2fd41d421d4a267075dff30ed94b7b1bb965
wcl1 <wcl1@meitu.com>
2019-06-19 20:31:04 +0800

    update

--- root_dir.py
+++ root_dir.py
@@ -8,4 +8,5 @@ Created by C. L. Wang on 2018/4/23
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # 存储项目所在的绝对路径
-DATA_DIR = '/Users/wang/workspace/data_set/person-test'
+# DATA_DIR = '/Users/wang/workspace/data_set/person-test'
+DATA_DIR = os.path.join(ROOT_DIR, 'data', 'person-test')
